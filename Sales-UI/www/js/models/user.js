fbf56890ec6fbe29d0b74768be6ad751bf27a2a7
Pawandeep Singh <pawandeep2007@gmail.com>
2017-01-02 01:42:45 +0530

    user.js model created. To decide between user.js vs login.js + register.js models. Styling of register and login done.

---
+++ Sales-UI/www/js/models/user.js
@@ -0,0 +1,46 @@
+define(['backbone', 'config', 'jquery'], function (Backbone, Config, $) {
+    var user = Backbone.Model.extend({
+        defaults: {
+            firstName: '',
+            lastName: '',
+            username: '',
+            password: '',
+            mobileNumber: '',
+            emailId: ''
+        },
+        findByMobileNumber: function () {
+            $.ajax({
+                url: Config.path + '/user',
+                param: {
+                    mobileNumber: mobileNumber
+                }
+            }).done(function (data) {
+                console.log('findByMobileNumber ' + data);
+            });
+        },
+        findByUsername: function () {
+            $.ajax({
+                url: Config.path + '/user',
+                param: {
+                    username: username
+                }
+            }).done(function (data) {
+                console.log('findByUsername ' + data);
+            });
+        },
+        findByEmailId: function () {
+            $.ajax({
+                url: Config.path + '/user',
+                param: {
+                    emailId: emailId
+                }
+            }).done(function (data) {
+                console.log('findByEmailId ' + data);
+            });
+        },
+        url: Config.path + '/user'
+
+    });
+
+    return user;
+});
