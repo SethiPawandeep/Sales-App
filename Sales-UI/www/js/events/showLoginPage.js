b060e9c3b0fcbb16755e595651ad06d23c6be7f7
Pawandeep Singh <pawandeep2007@gmail.com>
2017-01-02 20:28:56 +0530

    Login Page and Register Page events written. User model user.

--- Sales-UI/www/js/events/showLoginPage.js
+++ Sales-UI/www/js/events/showLoginPage.js
@@ -1,18 +1,35 @@
 define(['app'], function(App) {
     App.commands.setHandler('showLoginPage', function() {
-        require(['views/login', 'models/login'], function(Login, LoginModel) {
+        require(['views/login', 'models/login', 'models/user'], function(Login, LoginModel, UserModel) {
             var login = new LoginModel(),
-                user = new LoginModel(),
+                user = new UserModel(),
                 loginView;
             App.Main.show(loginView = new Login({
                 model: login
             }));
-            loginView.listenTo(loginView, 'onRegister', function(){
-                console.log('asdfasdfasdfasdfasdf');
-                App.execute('showRegisterPage'); 
-                console.log('asdfasdfasdfasdfasdf');
+            loginView.listenTo(loginView, 'login', function() {
+                user.fetch({
+                    data: {
+                        findByUsername: login.get('username');
+                    } 
+                }).done(function(){
+                    if(login.get('password') === user.get('password')) {
+                        //Open app home page
+                    } else {
+                        alert('Invalid credentials');
+                        /*
+                            1. Develop a way to check if the username exists or not.
+                            2. Use something else in place of alert.
+                        */
+                        
+                    }
+                }).fail(function(){
+                    alert('Error communicating server')   
+                });
             });
-  /**/
+            loginView.listenTo(loginView, 'onRegister', function(){
+                App.execute('showRegisterPage'); 
+            });
         }); 
     });
 });
--- Sales-UI/www/js/events/showRegisterPage.js
+++ Sales-UI/www/js/events/showRegisterPage.js
@@ -1,12 +1,29 @@
 define(['app'], function (App) {
     App.commands.setHandler('showRegisterPage', function () {
-        require(['views/register', 'models/register'], function(Register, RegisterModel) {
-            var user = new RegisterModel(),
+        require(['views/register', 'models/register', 'models/user'], function (Register, RegisterModel, UserModel) {
+            var user = new UserModel(),
+                register = new RegisterModel(),
                 registerView;
             App.Main.show(registerView = new Register({
-                model: user
+                model: register
             }));
-/**/
+            registerView.listenTo(registerView, 'register', function () {
+                user.fetch({
+                    data: {
+                        findByMobileNumber: register.get('phoneNumber')
+                    }
+                }).done(function () {
+                    if (register.get('phoneNumber') === user.get('phoneNumber') || register.get('emailId') === user.get('emailId')) {
+                        alert('User with the entered credentials already exists\n');
+                        //User already exists
+                    } else {
+                        App.execute('setUsernameAndConfirm', register);
+                    }
+                }).fail(function(){
+                    alert('Error communication server.');
+                });
+            });
+            /**/
         });
     });
 });
